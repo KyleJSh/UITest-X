33929ee00aabde24ddeb4412c46c30aaf6bbb942
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-16 19:54:41 -0800

    Initial Commit

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello world!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color.black)
+            .multilineTextAlignment(.leading)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .background(Color.green.blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
+
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
