d47a8a2c33ba8d1562e45b6752f4fc314427992a
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-16 16:37:51 -0800

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Kyle Sherrington on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
